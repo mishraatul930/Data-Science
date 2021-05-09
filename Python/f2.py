8fea1cd8b1803015ca4c2cd30b4399505a7861cf
Atul Mishra <mishraatul930@gmail.com>
2021-05-09 16:51:10 +0530

    Second commit

---
+++ Python/f2.py
@@ -0,0 +1,17 @@
+a = 0b1010
+print(a)
+print(oct(a))
+No concept for long or short 
+to check the size we use import sys
+
+import sys
+a = 10
+print(sys.getsizeof(a))
+
+no concept of double in float
+complex_num = 10+20j
+print(a.real)
+print(a.imag)
+
+#Comparison between two different datatypes is not possible
+#Comparison between complex number is not possible
